ab08f0899ec5b2aaced27bd4304eaf4ffd18250e
yournamesisyphus61 <username@example.com>
2018-07-26 23:05:56 +0800

    version 0.12

--- spider_acfun.py
+++ spider_acfun.py
@@ -1,8 +1,5 @@
 import requests
 from urllib.parse import urlencode
-from multiprocessing.pool import Pool
-import csv
-import pymysql
 import time
 
 headers = {
@@ -10,11 +7,21 @@ headers = {
                   'Chrome/67.0.3396.99 Safari/537.36'
 }
 
-def get_page(url):
+def get_channelurl(pageNo, realmIds):
+    params = {
+        'pageNo': pageNo,
+        'size': '10',
+        'realmIds': realmIds,
+        'originalOnly': 'false',
+        'orderType': '2',
+        'periodType': '-1',
+        'filterTitleImage': 'true'
+    }
+    url = 'http://webapi.aixifan.com/query/article/list?' + urlencode(params)
     try:
-        page = requests.get(url, headers=headers)
-        if page.status_code == 200:
-            return page.json()
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json()
     except requests.ConnectionError:
         return None
 
@@ -22,88 +29,36 @@ def get_info(json):
     if json.get('data'):
         item = json.get('data')
         if item.get('articleList'):
+            totalPage = item.get('totalPage')
             for info in item.get('articleList'):
                 title = info.get('title')
                 view_count = info.get('view_count')
                 comment_count = info.get('comment_count')
                 item = {
+                    'totalPage': totalPage,
                     'title': title,
                     'view_count': view_count,
                     'comment_count': comment_count
                 }
-                print(item)
-                db = pymysql.connect(host='localhost', user='root', passwd='******', port=3306,
-                                     db='spiders')
-                cursor = db.cursor()
-                sql_1 = 'CREATE TABLE IF NOT EXISTS acfun (' \
-                        'title VARCHAR(255) NOT NULL, ' \
-                        'view_count INT NOT NULL, ' \
-                        'comment_count INT NOT NULL, ' \
-                        'PRIMARY KEY (title))'
-                cursor.execute(sql_1)
+                return item
 
-                table = 'acfun'
-                keys = ', '.join(item.keys())
-                values = ', '.join(['%s'] * len(item))
-                sql = 'INSERT INTO {table}({keys}) VALUES ({values})'.format(table=table, keys=keys, values=values)
-                try:
-                    if cursor.execute(sql, tuple(item.values())):
-                        print('Successful')
-                        db.commit()
-                except:
-                    print('Failed')
-                    db.rollback()
-                db.close()
-
-'''
 def save_data(item):
-    db = pymysql.connect(host='localhost', user='root', passwd='lww890226', port=3306,
-                         db='spiders')
-    cursor = db.cursor()
-    sql = 'INSERT INTO acfun (title,view_count,comment_count) VALUES (%s,%s,%s)'
-    try:
-        cursor.execute(sql, (item['title'], item['view_count'], item['comment_count']))
-        print('Successful')
-        db.commit()
-    except:
-        print('Failed')
-        db.rollback()
-    db.close()
-    '''
+    print(item)
 
-'''
-    table = 'acfun_articleList'
-    keys = ', '.join(item.keys())
-    values = ', '.join(['%s'] * len(item))
-    sql = 'INSERT INTO {table}({keys}) VALUES ({values})'.format(table=table, keys=keys, values=values)
-    try:
-        if cursor.execute(sql, tuple(item.values())):
-            print('Successful')
-            db.commit()
-    except:
-        print('Failed')
-        db.rollback()
-    db.close()
-'''
+def main():
+    for realmIds in range(1, 24):
+        pageNo = 1
+        json_start = get_channelurl(pageNo, realmIds)
+        item = get_info(json_start)
+        try:
+            sum = item.get('totalPage')
+            for pageNo in range(1, int(sum)):
+                json = get_channelurl(pageNo, realmIds)
+                info = get_info(json)
+                save_data(info)
+        except AttributeError :
+            pass
 
-'''
-                filename = 'acfun.csv'
-                with open(filename, 'a') as csvfile:
-                    fieldnames = ['title', 'view_count', 'comment_count']
-                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                    writer.writeheader()
-                    writer.writerow({'title': title, 'view_count': view_count, 'comment_count': comment_count})
-
-                    # f.write('标题：' + item['title'] + ',' + '点击量：' + str(item['view_count']) + ',' + str(item['comment_count'] + '\n'))
-'''
 
 if __name__ == '__main__':
-
-    h = 'http://webapi.aixifan.com/query/article/list?' \
-        'pageNo={}&size=10&realmIds=5%2C22%2C1%2C2%2C4' \
-        '&originalOnly=false&orderType=2&periodType=-1&filterTitleImage=true'
-    urls = [h.format(str(i)) for i in range(1, 11)]
-    for url in urls:
-        json = get_page(url)
-        get_info(json)
-
+    main()
